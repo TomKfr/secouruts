fba79c5cbc79a4388ada99ef607fd2025e185f0d
becretmo <morgane.becret@hotmail.fr>
2015-05-19 09:56:19 +0200

    ajout diplome dans creneaux

--- app/dps_controller.php
+++ app/dps_controller.php
@@ -66,7 +66,7 @@ class DPSController implements ControllerProviderInterface
 					$item = array();
 					$item['time'] = $creneau->getDateDeb()->format('H:i')." - ".$creneau->getDateFin()->format('H:i');
 					foreach ($creneau->getSecouristes() as $user) {
-						$item[$user->getLogin()] = $user->getPrenom()." ".$user->getNom();
+						$item[$user->getLogin()] = $user->getPrenom()." ".$user->getNom()."(".$user->getBestDiplome().")";
 					}
 					$crenos[$creneau->getId()] = $item;
 				}
